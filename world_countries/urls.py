095de6038990932a95d54a53c3c6caf58112712e
AnFlGue <mokshaflores@icloud.com>
2024-08-20 20:49:33 +0200

    02 Commit: added index.html and serializers.py

--- world_countries/urls.py
+++ world_countries/urls.py
@@ -1,7 +1,11 @@
+# world_countries/urls.py
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from countries.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('countries/', include('countries.urls')),
+    path('', index, name='index'),  # Add this line
 ]
